7998f3fd3303a0d11e927368c2ec7b2cb6cdb175
sw10pa <stephanegurgenidze@gmail.com>
2025-06-11 19:48:36 +0400

    Rename counter program to voting

---
+++ 02-voting-dapp/anchor/programs/voting/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
+
+#[program]
+pub mod voting {
+    use super::*;
+}
